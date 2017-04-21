a3b74b932e8d00680c1e068fcd55157b032a1673
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-04-21 15:00:28 +0100

    Changed version of Jenkins

--- packer/jenkins/scripts/jenkins.sh
+++ packer/jenkins/scripts/jenkins.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Fetching Jenkins..."
-sudo curl -L -o jenkins.deb http://mirrors.jenkins-ci.org/debian/jenkins_2.50_all.deb
+sudo curl -L -o jenkins.deb http://mirrors.jenkins-ci.org/debian/jenkins_2.55_all.deb
 
 echo "Installing Jenkins..."
 sudo apt-get install -y ./jenkins.deb
