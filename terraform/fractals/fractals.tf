ec1d9c3b6aac23812b6cbabb3a79dbc9d159de14
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-06-04 17:18:45 +0100

    Added access to fractals archive

--- terraform/fractals/fractals.tf
+++ terraform/fractals/fractals.tf
@@ -167,6 +167,18 @@ resource "aws_iam_role_policy" "lambda_role_policy" {
               "logs:PutLogEvents"
           ],
           "Resource": "arn:aws:logs:*:*:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::fractals-archive/*"
+            ]
         }
     ]
 }
