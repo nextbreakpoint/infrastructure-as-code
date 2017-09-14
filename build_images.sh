bb0cc853f18b9c2ace8d59c807b1abb016777ee7
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-09-14 08:05:40 +0100

    Refactor build images script

--- build_images.sh
+++ build_images.sh
@@ -11,4 +11,46 @@ popd
 
 echo "{\"aws_vpc_id\":\"$VPC\",\"aws_subnet_id\":\"$SUBNET\"}" > network_vars.json
 
-cd $DIR/packer && sh build.sh
+# Create base image
+cd $DIR/packer/base && pk_create
+
+# Create images in parallel
+
+cd $DIR/packer/pipeline && pk_create &
+pipeline_pid=$!
+
+cd $DIR/packer/puppet && pk_create &
+puppet_pid=$!
+
+cd $DIR/packer/elasticsearch && pk_create &
+elasticsearch_pid=$!
+
+cd $DIR/packer/logstash && pk_create &
+logstash_pid=$!
+
+cd $DIR/packer/kibana && pk_create &
+kibana_pid=$!
+
+cd $DIR/packer/zookeeper && pk_create &
+zookeeper_pid=$!
+
+cd $DIR/packer/cassandra && pk_create &
+cassandra_pid=$!
+
+cd $DIR/packer/kafka && pk_create &
+kafka_pid=$!
+
+cd $DIR/packer/kubernetes && pk_create &
+kubernetes_pid=$!
+
+# Await termination
+
+wait $pipeline_pid
+wait $puppet_pid
+wait $elasticsearch_pid
+wait $logstash_pid
+wait $kibana_pid
+wait $zookeeper_pid
+wait $cassandra_pid
+wait $kafka_pid
+wait $kubernetes_pid
