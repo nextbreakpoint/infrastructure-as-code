64c51255ec8ede77bcd4fdbaa7a68519cc9efff4
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-10-31 23:02:36 +0000

    WIP

--- docker/consul/generate.sh
+++ docker/consul/generate.sh
@@ -2,8 +2,8 @@
 
 echo '[extended]\nextendedKeyUsage=serverAuth,clientAuth' > openssl.cnf
 
-openssl req -newkey rsa:2048 -days 365 -x509 -nodes -out /output/consul_ca.cer -keyout /output/consul_ca.pem -passin pass:secret -passout pass:secret -subj "/CN=myself/OU=/O=/L=/ST=/C=/"
+openssl req -newkey rsa:2048 -days 365 -x509 -nodes -out /output/consul_ca_cert.pem -keyout /output/consul_ca_key.pem -passin pass:secret -passout pass:secret -subj "/CN=myself/OU=/O=/L=/ST=/C=/"
 
-openssl req -newkey rsa:1024 -nodes -out /output/consul_server.csr -keyout /output/consul_server.key -passin pass:secret -passout pass:secret -subj "/CN=server.terraform.consul/OU=/O=/L=/ST=/C=/"
+openssl req -newkey rsa:1024 -nodes -out /output/consul_server_cert.csr -keyout /output/consul_server_key.pem -passin pass:secret -passout pass:secret -subj "/CN=server.terraform.consul/OU=/O=/L=/ST=/C=/"
 
-openssl x509 -req -extfile openssl.cnf -extensions extended -days 365 -CAcreateserial -CA /output/consul_ca.cer -CAkey /output/consul_ca.pem -in /output/consul_server.csr -out /output/consul_server.pem -passin pass:secret
+openssl x509 -req -extfile openssl.cnf -extensions extended -days 365 -CAcreateserial -CA /output/consul_ca_cert.pem -CAkey /output/consul_ca_key.pem -in /output/consul_server_cert.csr -out /output/consul_server_cert.pem -passin pass:secret
--- generate_certificates.sh
+++ generate_certificates.sh
@@ -47,20 +47,28 @@ keytool -noprompt -keystore $DIR/truststore-client.jks -alias CARoot -import -fi
 ## Import CA into server truststore
 keytool -noprompt -keystore $DIR/truststore-server.jks -alias CARoot -import -file $DIR/ca-cert -storepass secret
 
-## Create PEM files for NGINX
+## Create PEM files for NGINX, Logstash and Filebeat
 
 ### Extract signed client certificate
-keytool -noprompt -keystore $DIR/keystore-server.jks -exportcert -alias selfsigned -rfc -storepass secret -file $DIR/server_cert.pem
+keytool -noprompt -keystore $DIR/keystore-client.jks -exportcert -alias selfsigned -rfc -storepass secret -file $DIR/client_cert.pem
 
 ### Extract client key
-keytool -noprompt -srckeystore $DIR/keystore-server.jks -importkeystore -srcalias selfsigned -destkeystore $DIR/cert_and_key.p12 -deststoretype PKCS12 -srcstorepass secret -storepass secret
-openssl pkcs12 -in $DIR/cert_and_key.p12 -nocerts -nodes -passin pass:secret -out $DIR/server_key.pem
+keytool -noprompt -srckeystore $DIR/keystore-client.jks -importkeystore -srcalias selfsigned -destkeystore $DIR/client_cert_and_key.p12 -deststoretype PKCS12 -srcstorepass secret -storepass secret
+openssl pkcs12 -in $DIR/client_cert_and_key.p12 -nocerts -nodes -passin pass:secret -out $DIR/client_key.pem
+
+### Extract signed server certificate
+keytool -noprompt -keystore $DIR/keystore-server.jks -exportcert -alias selfsigned -rfc -storepass secret -file $DIR/server_cert.pem
+
+### Extract server key
+keytool -noprompt -srckeystore $DIR/keystore-server.jks -importkeystore -srcalias selfsigned -destkeystore $DIR/server_cert_and_key.p12 -deststoretype PKCS12 -srcstorepass secret -storepass secret
+openssl pkcs12 -in $DIR/server_cert_and_key.p12 -nocerts -nodes -passin pass:secret -out $DIR/server_key.pem
 
 ### Extract CA certificate
 keytool -noprompt -keystore $DIR/keystore-server.jks -exportcert -alias CARoot -rfc -storepass secret -file $DIR/ca_cert.pem
 
 ### Copy keystores and truststores
 
+cat $DIR/client_cert.pem $DIR/ca_cert.pem > $DIR/ca_and_client_cert.pem
 cat $DIR/server_cert.pem $DIR/ca_cert.pem > $DIR/ca_and_server_cert.pem
 
 export DST=terraform/secrets/environments/production/keystores
@@ -73,7 +81,7 @@ cp $DIR/keystore-server.jks $DST
 cp $DIR/truststore-client.jks $DST
 cp $DIR/truststore-server.jks $DST
 
-### Copy CA and server certificates and keys
+### Copy certificates and keys
 
 export DST=terraform/secrets/environments/production/nginx
 
@@ -83,3 +91,31 @@ cp $DIR/ca_cert.pem $DST
 cp $DIR/server_cert.pem $DST
 cp $DIR/server_key.pem $DST
 cp $DIR/ca_and_server_cert.pem $DST
+
+export DST=terraform/secrets/environments/production/logstash
+
+mkdir -p $DST
+
+cp $DIR/ca_cert.pem $DST
+cp $DIR/server_cert.pem $DST
+cp $DIR/server_key.pem $DST
+
+export DST=terraform/secrets/environments/production/filebeat
+
+mkdir -p $DST
+
+cp $DIR/ca_cert.pem $DST
+cp $DIR/client_cert.pem $DST
+cp $DIR/client_key.pem $DST
+
+docker build -t configure-consul docker/consul/.
+docker run -t -v $(pwd)/$DIR:/output configure-consul
+docker run -t -v $(pwd)/$DIR:/output configure-consul openssl x509 -noout -text -in /output/consul_server_cert.pem
+
+export DST=terraform/secrets/environments/production/consul
+
+mkdir -p $DST
+
+cp $DIR/consul_ca_cert.pem $DST/ca_cert.pem
+cp $DIR/consul_server_cert.pem $DST/server_cert.pem
+cp $DIR/consul_server_key.pem $DST/server_key.pem
