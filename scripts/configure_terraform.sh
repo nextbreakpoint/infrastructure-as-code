dc4413ebd0614d5f482faf45ded4677a049ad227
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-12-29 13:49:14 +0000

    Add reset script

--- scripts/configure_terraform.sh
+++ scripts/configure_terraform.sh
@@ -7,6 +7,4 @@ echo "Region = "$2
 
 bash -c "$(find terraform -name "remote_state.tf" -exec echo "sed -i '.bkp' 's/bucket = \".*\"/bucket = \"$1\"/g; s/region = \".*\"/region = \"$2\"/g' {}" \;)"
 
-#find . -name "*.tf.bkp" -exec rm {} \;
-
 echo "done."
---
+++ scripts/reset_terraform.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+find . -name "tfplan" -exec rm {} \;
+find . -name "*.tf.bkp" -exec rm {} \;
+find . -name "*.tfstate.backup" -exec rm {} \;
+find . -name ".terraform" -exec rm -fR {} \;
+
+rm -fR terraform/secrets/environments secrets config/consult.tfvars config/network_vars.json
