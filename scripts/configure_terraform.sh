062f77a4107881d83c9058f41b9ae24fd4e0a22f
Andrea Medeghini <andrea@nextbreakpoint.com>
2017-12-29 14:00:59 +0000

    Reset backend defaults

--- scripts/configure_terraform.sh
+++ scripts/configure_terraform.sh
@@ -5,6 +5,6 @@ echo "Configuring Terraform..."
 echo "Bucket = "$1
 echo "Region = "$2
 
-bash -c "$(find terraform -name "remote_state.tf" -exec echo "sed -e 's/bucket = \".*\"/bucket = \"$1\"/g; s/region = \".*\"/region = \"$2\"/g' {}" \;)"
+bash -c "$(find terraform -name "remote_state.tf" -exec echo "sed -i.backup 's/bucket = \".*\"/bucket = \"$1\"/g; s/region = \".*\"/region = \"$2\"/g' {}" \;)"
 
 echo "done."
--- scripts/reset_terraform.sh
+++ scripts/reset_terraform.sh
@@ -2,6 +2,8 @@
 
 find . -name "tfplan" -exec rm {} \;
 find . -name "*.tf.bkp" -exec rm {} \;
+find . -name "*.tfbkp" -exec rm {} \;
+find . -name "*.tf.backup" -exec rm {} \;
 find . -name "*.tfstate.backup" -exec rm {} \;
 find . -name ".terraform" -exec rm -fR {} \;
 
