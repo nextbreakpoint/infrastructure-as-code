07f1118e5960de5544d05babbfc00529faa9af71
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-06-16 12:02:08 +0100

    add consul

--- swarm/swarm-run.sh
+++ swarm/swarm-run.sh
@@ -6,6 +6,9 @@ export DOCKER_MASTER=$(docker-machine ip docker-master)
 export DOCKER_HOST_1=$(docker-machine ip docker-worker1)
 export DOCKER_HOST_2=$(docker-machine ip docker-worker2)
 export DOCKER_HOST_3=$(docker-machine ip docker-worker3)
+export SECRETS_PATH=$(pwd)/../secrets/generated
+export CONSUL_SECRET=$(cat $(pwd)/../config/consul.tfvars | jq -r ".consul_secret")
+export CONSUL_DATACENTER=internal
 
 eval $(docker-machine env $1)
 
