e72cd3fe9da976a209067c1ce9f1307d7836f7d4
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-06-16 15:33:30 +0100

    move secrets

--- swarm/test-produce.sh
+++ swarm/test-produce.sh
@@ -3,9 +3,9 @@
 docker run --rm -it --net=services \
   -v $SECRETS_PATH/kafka1-server-keystore.jks:/secrets/keystore.jks \
   -v $SECRETS_PATH/kafka1-server-truststore.jks:/secrets/truststore.jks \
-  -v $(pwd)/password_keystore.txt:/secrets/password_keystore.txt \
-  -v $(pwd)/password_truststore.txt:/secrets/password_truststore.txt \
-  -v $(pwd)/client-ssl.properties:/secrets/client-ssl.properties \
+  -v $(pwd)/secrets/password_keystore.txt:/secrets/password_keystore.txt \
+  -v $(pwd)/secrets/password_truststore.txt:/secrets/password_truststore.txt \
+  -v $(pwd)/secrets/client-ssl.properties:/secrets/client-ssl.properties \
   $KAFKA_IMAGE \
   /opt/kafka_2.11-$KAFKA_VERSION/bin/kafka-console-producer.sh \
   --broker-list kafka1:9092 \
