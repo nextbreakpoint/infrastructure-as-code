a620fd34f28ba6bf7cebfb3fb8f6498f23fc85f7
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-07-01 08:42:54 +0100

    remove docker host variables

---
+++ swarm/configure-jsonfile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+docker-machine scp daemon-jsonfile.json docker-master:
+docker-machine scp daemon-jsonfile.json docker-worker1:
+docker-machine scp daemon-jsonfile.json docker-worker2:
+docker-machine scp daemon-jsonfile.json docker-worker3:
+
+docker-machine ssh docker-master sudo mv daemon-jsonfile.json /etc/docker/daemon.json
+docker-machine ssh docker-worker1 sudo mv daemon-jsonfile.json /etc/docker/daemon.json
+docker-machine ssh docker-worker2 sudo mv daemon-jsonfile.json /etc/docker/daemon.json
+docker-machine ssh docker-worker3 sudo mv daemon-jsonfile.json /etc/docker/daemon.json
+
+docker-machine ssh docker-master sudo cat /etc/docker/daemon.json
+docker-machine ssh docker-worker1 sudo cat /etc/docker/daemon.json
+docker-machine ssh docker-worker2 sudo cat /etc/docker/daemon.json
+docker-machine ssh docker-worker3 sudo cat /etc/docker/daemon.json
+
+docker-machine ssh docker-master sudo /etc/init.d/docker restart
+docker-machine ssh docker-worker1 sudo /etc/init.d/docker restart
+docker-machine ssh docker-worker2 sudo /etc/init.d/docker restart
+docker-machine ssh docker-worker3 sudo /etc/init.d/docker restart
