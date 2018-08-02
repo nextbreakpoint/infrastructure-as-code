a15c3bd20c24e83ad2828fe7a149906f2428f27b
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-08-02 21:31:57 +0100

    update artifcatory

--- docker/artifactory/publish.sh
+++ docker/artifactory/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export ARTIFACTORY_VERSION=5.4.6
+export ARTIFACTORY_VERSION=6.1.0
 export ARTIFACTORY_REVISION=1
 
 docker tag nextbreakpoint/artifactory:$ARTIFACTORY_VERSION nextbreakpoint/artifactory:$ARTIFACTORY_VERSION-$ARTIFACTORY_REVISION && docker push nextbreakpoint/artifactory:$ARTIFACTORY_VERSION-$ARTIFACTORY_REVISION
