a2e663dcedb2597fb6fa07df70e332b99a07dee0
Andrea Medeghini <andrea@nextbreakpoint.com>
2018-08-04 10:10:25 +0100

    remove output

--- terraform/bastion/outputs.tf
+++ terraform/bastion/outputs.tf
@@ -2,9 +2,9 @@
 # Outputs
 ##############################################################################
 
-output "bastion-server-a-public-ip" {
-  value = "${module.bastion_a.public-ips}"
-}
+# output "bastion-server-a-public-ip" {
+#   value = "${module.bastion_a.public-ips}"
+# }
 
 # output "bastion-server-b-public-ip" {
 #   value = "${module.bastion_b.public-ips}"
